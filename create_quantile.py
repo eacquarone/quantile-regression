b2b996cc09edf33362a469282fe73bef7e35e908
HichamEB <hicham.el.boukkouri@ensae.fr>
2017-12-05 18:04:57 +0100

    pep8 create_quantile.py

--- create_quantile.py
+++ create_quantile.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
+
 def main():
     census_data = pd.read_stata("Data/census80.dta")
 
-    for i in [10,25,50,75,90]:
-        census_data["cqlogwk_q%i"%i] = census_data.logwk
+    for i in [10, 25, 50, 75, 90]:
+        census_data["cqlogwk_q%i" % i] = census_data.logwk
 
-    census_data_cq = census_data[['educ', 'cqlogwk_q10', u'cqlogwk_q25', u'cqlogwk_q50', u'cqlogwk_q75',
-                                  u'cqlogwk_q90']]
+    columns = [
+        'educ', 'cqlogwk_q10', 'cqlogwk_q25',
+        'cqlogwk_q50', 'cqlogwk_q75', 'cqlogwk_q90'
+    ]
+
+    census_data_cq = census_data[columns]
 
     result = census_data_cq.groupby('educ').agg({
         'cqlogwk_q10': (lambda x: x.quantile(.1)),
@@ -19,7 +24,6 @@ def main():
 
     result.to_csv("Data/census80cq.csv")
 
+
 if __name__ == '__main__':
     main()
-
-
