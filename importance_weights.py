52f14fb1862f922963d8d580bdd83dcd70c68828
HichamEB <hicham.el.boukkouri@ensae.fr>
2017-12-06 14:47:42 +0100

    WIP - not getting the right values yet

--- importance_weights.py
+++ importance_weights.py
@@ -1,23 +1,44 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from sklearn.neighbors import KernelDensity
 
 
 def main():
-    data = pd.read_stata("Data/census80qr.dta")
-    group = data.groupby("educ")
-    print(group.head())
+    """Computes importance weights."""
 
-    generated_columns = {}
-    for tau in [10, 25, 50, 75, 90]:
-        for educ in range(5, 20 + 1):
-            name = "epsilon{}_q{}".format(educ, tau)
-            delta_name = "delta_q{}".format(tau)
+    data = pd.read_csv("Data/census80delta.csv")
+    data_qr = pd.read_stata("Data/census80qr.dta")
+    groups = data_qr.groupby("educ")
 
-            delta = group.get_group(educ)[delta_name]
-            assert len(set([v for v in delta.values])) == 1
-            delta = delta.values[0]
+    for tau in tqdm([10, 25, 50, 75, 90]):
+        for educ in tqdm(range(5, 20 + 1)):
+            kde = KernelDensity(kernel='gaussian')
+            estimation_points = groups.get_group(educ)
+            estimation_points = estimation_points["epsilon_q{}".format(tau)]
+            kde.fit(estimation_points.values.reshape(-1, 1))
 
-            generated_columns[name] = delta * np.linspace(0, 1, 100)
+            s = "{}_q{}".format(educ, tau)
+            data["epsilon" + s] = data["delta" + s] * (data["u"] - 1) / 100.
+
+            data["wdensity" + s] = 1 - (data["u"] - 1) / 100.
+            data["wdensity" + s] *= np.exp(
+                kde.score_samples(data["epsilon" + s].values.reshape(-1, 1))
+            )
+
+    means = data.mean(axis=0)
+    for tau in tqdm([10, 25, 50, 75, 90]):
+        data_qr["wdensity_q{}".format(tau)] = 0
+
+        for educ in tqdm(range(5, 20 + 1)):
+            s = "{}_q{}".format(educ, tau)
+            idx = groups.get_group(educ).index
+
+            data_qr.loc[
+                idx, "wdensity_q{}".format(tau)
+            ] = means["wdensity" + s]
+
+    print(groups.mean()[[c for c in data_qr if "density" in c]])
 
     # WIP
 
