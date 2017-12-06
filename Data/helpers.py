9a2e16c08ac2968d3065d45f193261e434e0a983
TheDoctor <aenguerand@yahoo.fr>
2017-12-06 15:19:41 +0100

    Added silverman rule of thumb

---
+++ Data/helpers.py
@@ -0,0 +1,11 @@
+def silverman_factor(x):
+    '''
+
+    :param x: A pandas.DataFrame column
+    :return: the ideal bandwidth according to silverman rule of thumb
+    '''
+    iqr = x.quantile(.75)-x.quantile(.25)
+    m = min(x.std()**2, iqr/1.349)
+    n = len(x)
+    return (.9*m)/n**(.2)
+
